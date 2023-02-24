47677e5d36354b078549da886104ad8b48ec23d4
Sebastian Müller <gsenpo@gmail.com>
2023-02-24 19:28:23 +0100

    Add arrays::left_rotation.rs

---
+++ arrays/left_rotation.rs
@@ -0,0 +1,58 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'rotLeft' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER d
+ */
+
+fn rotLeft(a: &[i32], d: i32) -> Vec<i32> {
+    let mut a_rotated = a.to_vec();
+    a_rotated.rotate_left(d as usize);
+    a_rotated
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let first_multiple_input: Vec<String> = stdin_iterator
+        .next()
+        .unwrap()
+        .unwrap()
+        .split(' ')
+        .map(|s| s.to_string())
+        .collect();
+
+    let n = first_multiple_input[0].trim().parse::<i32>().unwrap();
+
+    let d = first_multiple_input[1].trim().parse::<i32>().unwrap();
+
+    let a: Vec<i32> = stdin_iterator
+        .next()
+        .unwrap()
+        .unwrap()
+        .trim_end()
+        .split(' ')
+        .map(|s| s.to_string().parse::<i32>().unwrap())
+        .collect();
+
+    let result = rotLeft(&a, d);
+
+    for i in 0..result.len() {
+        write!(&mut fptr, "{}", result[i]).ok();
+
+        if i != result.len() - 1 {
+            write!(&mut fptr, " ").ok();
+        }
+    }
+
+    writeln!(&mut fptr).ok();
+}
