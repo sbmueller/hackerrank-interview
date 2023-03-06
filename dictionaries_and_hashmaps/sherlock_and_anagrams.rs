8884f492f3d42dad8685defa32b177e2a754bc64
Sebastian Müller <gsenpo@gmail.com>
2023-03-06 18:50:54 +0100

    Add dictionaries_and_hashmaps::sherlock_and_anagrams

---
+++ dictionaries_and_hashmaps/sherlock_and_anagrams.rs
@@ -0,0 +1,56 @@
+use std::collections::HashMap;
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'sherlockAndAnagrams' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+/// Sort a string by its characters
+fn sort_string(s: &str) -> String {
+    let mut chars: Vec<char> = s.chars().collect();
+    chars.sort_unstable();
+    chars.into_iter().collect()
+}
+
+fn sherlock_and_anagrams(s: &str) -> i32 {
+    let mut anagrams: HashMap<String, i32> = HashMap::new();
+    let chars: Vec<char> = s.chars().collect();
+    for l in 1..s.len() {
+        chars
+            .windows(l) // windows of size l over the input string
+            .map(|v| v.iter().collect::<String>()) // rebuild substrings from char arrays
+            .for_each(|substring| {
+                *anagrams.entry(sort_string(&substring)).or_insert(-1) += 1; // increase substring
+                                                                             // counter
+            });
+    }
+    anagrams.values().map(|&v| (0..=v).sum::<i32>()).sum() // Sum over all anagram possibilities
+                                                           // and all anagrams
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let q = stdin_iterator
+        .next()
+        .unwrap()
+        .unwrap()
+        .trim()
+        .parse::<i32>()
+        .unwrap();
+
+    for _ in 0..q {
+        let s = stdin_iterator.next().unwrap().unwrap();
+
+        let result = sherlock_and_anagrams(&s);
+
+        writeln!(&mut fptr, "{}", result).ok();
+    }
+}
